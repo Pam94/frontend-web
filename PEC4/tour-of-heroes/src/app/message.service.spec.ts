587fd567536c8f908420cdf4a336629e8ac04fb0
Pam94 <pamelafernandez94@gmail.com>
2021-01-01 19:11:16 +0100

    MessageService tests completed -Ej. 3-

--- PEC4/tour-of-heroes/src/app/message.service.spec.ts
+++ PEC4/tour-of-heroes/src/app/message.service.spec.ts
@@ -12,5 +12,21 @@ describe('MessageService', () => {
             expect(service.messages.length).toBe(actualLength + 1);
             expect(service.messages[service.messages.length - 1]).toBe(string);
             done();
-        })
+        });
+
+    it('#clear when the message list is empty',
+        (done: DoneFn) => {
+            service.messages = [];
+            service.clear();
+            expect(service.messages.length).toBe(0);
+            done();
+        });
+
+    it('#clear when the message list is not empty',
+        (done: DoneFn) => {
+            service.messages = ['message 1', 'message 2', 'message 3'];
+            service.clear();
+            expect(service.messages.length).toBe(0);
+            done();
+        });
 });
